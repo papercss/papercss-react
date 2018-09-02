7caabf4c762ce23be98bd64b3901bf11abe26464
hasparus <hasparus@gmail.com>
2018-09-02 20:10:23 +0200

    Try to enhance examples.

---
+++ examples/src/examples/PaperInput.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { PaperInput } from "react-paper-css";
+
+import Example from "../Example";
+
+const Ex = (
+  <>
+    A standard input with no label.
+    <PaperInput />
+    Block Level 100% width input.
+    <PaperInput block />
+  </>
+);
+
+const PaperButton = () => (
+  <Example
+    name="PaperButton"
+    initialSource={`import React from "react";
+import { PaperButton } from "react-paper-css";
+  
+const Example = <PaperButton>Click me!</PaperButton>;
+
+export default Example;
+`}
+  />
+);
+
+export default PaperButton;
--- examples/src/examples/index.ts
+++ examples/src/examples/index.ts
@@ -1,1 +1,6 @@
-export { default as PaperButton } from "./PaperButton";
+import fs from "fs";
+
+declare var __dirname: string;
+
+const moduleAsString = (path: string) => fs.readFileSync(`${__dirname}${path}`);
+export const PaperButton = moduleAsString("./PaperButton.tsx");
