d3a49a0ad1dec806a31aaeb5946a7f5c908b2039
hasparus <hasparus@gmail.com>
2018-09-03 22:12:22 +0200

    Add Typography example.

--- examples/src/examples/Typography.tsx
+++ examples/src/examples/Typography.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { PaperTypography } from "react-paper-css";
 
-const Example = <></>;
+const Example = (
+  <PaperTypography>
+    <h1>Header</h1>
+    Body.
+  </PaperTypography>
+);
 
 export default Example;
