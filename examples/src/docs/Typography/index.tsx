81c9df0d8ffed57eed2f1e990f83e3be20f2167d
hasparus <hasparus@gmail.com>
2018-10-03 11:13:48 +0200

    Add some copy to About page.

--- examples/src/docs/Typography/index.tsx
+++ examples/src/docs/Typography/index.tsx
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 import React from "react";
 import { Typography } from "react-paper-css";
+import { Link } from "react-router-dom";
 
 import { H3 } from "../../Headings";
 import Sandbox from "../../Sandbox";
@@ -10,7 +11,11 @@ const source = readFileSync(`${__dirname}/Example.tsx`, "utf-8");
 const TypographyDoc = () => (
   <Typography>
     <H3>Typography</H3>
-    <p>Typography component provides his children with text styles.</p>
+    <p>
+      Typography component provides his children with text styles. If you need
+      only some of the styles, take a look at <Link to="/Heading">Heading</Link>{" "}
+      and <Link to="/Text">Text</Link>
+    </p>
     <Sandbox source={source} />
   </Typography>
 );
--- src/Typography/Heading.tsx
+++ src/Typography/Heading.tsx
@@ -10,18 +10,22 @@ export type HeadingType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type HeadingProps = {
   as: HeadingType;
   children: React.ReactNode;
-} & HTMLAttributes<HTMLDivElement>;
+} & HTMLAttributes<HTMLHeadingElement>;
+
 class Heading extends React.Component<HeadingProps> {
   public static defaultProps: Pick<HeadingProps, "as"> = {
     as: "h1",
   };
+  
   public render() {
     const { as, className, ...props } = this.props;
     const ElementType = as;
+
     return (
-      <ElementType className={classNames(styles.heading, className)} {...props}>
-        {this.props.children}
-      </ElementType>
+      <ElementType
+        className={classNames(styles.heading, className)}
+        {...props}
+      />
     );
   }
 }
