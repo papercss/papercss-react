d66191b1d1840fd4a2bd8d8b97e147c0a3b19bd7
hasparus <hasparus@gmail.com>
2018-10-03 22:48:12 +0200

    Stop prefixing Form components.

--- library/src/Forms/index.ts
+++ library/src/Forms/index.ts
@@ -1,5 +1,5 @@
 export { default } from "./Forms";
 export { default as Input } from "./Input";
-export { default as PaperCheckbox } from "./Checkbox";
-export { default as PaperRadio } from "./Radio";
-export { default as PaperSelect } from "./Select";
+export { default as Checkbox } from "./Checkbox";
+export { default as Radio } from "./Radio";
+export { default as Select } from "./Select";
